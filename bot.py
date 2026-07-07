ba4ac6adfd422f6c508369293dcce1e889dfb43f
Sora0471 <i.sora0827@icloud.com>
2026-07-08 05:30:05 +0900

    Add janken command

--- bot.py
+++ bot.py
@@ -35,5 +35,48 @@ async def omikuji(ctx):
     result = random.choice(fortunes)
     await ctx.send(result)
 
+@bot.command()
+async def janken(ctx, PR_hand):
+    hand = [
+        "グー",
+        "チョキ",
+        "パー",
+    ]
+
+    if PR_hand not in hand:
+        await ctx.send("グー・チョキ・パーから選んでね！")
+        return
+    
+    CP_hand = random.choice(hand)
+
+    while PR_hand == CP_hand:
+        await ctx.send(
+            f"あなた: {PR_hand}\n"
+            f"Bot: {CP_hand}\n"
+            "あいこでした！あいこでしょ・・・！"
+        )
+        CP_hand = random.choice(hand)
+
+    
+    if (
+    (PR_hand == "グー" and CP_hand == "チョキ")
+    or
+    (PR_hand == "チョキ" and CP_hand == "パー")
+    or
+    (PR_hand == "パー" and CP_hand == "グー")
+    ):
+        result = "あなたの勝ちです✨"
+
+    else:
+        result = "あなたの負けです😭"
+
+
+
+    await ctx.send(
+        f"あなた: {PR_hand}\n"
+        f"Bot: {CP_hand}\n"
+        f"{result}"
+     )
+
 
 bot.run(os.getenv("TOKEN"))
